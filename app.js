f1afafe752c99d34d6d2028e6c50df05b7f903b8
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-08-25 23:36:39 +0200

    updated

--- app.js
+++ app.js
@@ -21,7 +21,7 @@
 
 // console.log(typeof person[0]);
 
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
 
 // fruits.push("Lemon");
 
@@ -71,55 +71,55 @@ const fruits = ["Banana", "Orange", "Apple", "Mango"];
 
 // 1.Declare an empty array;
 
-const dec =[1,2,3,"4",5,6,7];
-console.log(dec.length)
+// const dec =[1,2,3,"4",5,6,7];
+// console.log(dec.length)
 
 // 4.Get the first item, the middle item and the last item of the array
 
-console.log(dec[0]);
+// console.log(dec[0]);
 
-console.log(dec[dec.length -1]);
+// console.log(dec[dec.length -1]);
 
-console.log(dec[Math.floor(dec.length/2)]);
+// console.log(dec[Math.floor(dec.length/2)]);
 
 
 // 5.Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
 
-const mixedDataTypes =[1, "Kerim", true, [123], {} , "123", 3<5 ];
+// const mixedDataTypes =[1, "Kerim", true, [123], {} , "123", 3<5 ];
 
 // 6.Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
 
-console.log(typeof mixedDataTypes[mixedDataTypes.length-1]);
+// console.log(typeof mixedDataTypes[mixedDataTypes.length-1]);
 
-const itCompanies = []
+// const itCompanies = []
 
-itCompanies[0]= "Facebook";
-itCompanies[1]= "Google";
-itCompanies.push("Microsoft");
-itCompanies.push("Apple");
-itCompanies.push("IBM");
-itCompanies.push("Oracle");
-itCompanies.push("Amazon");
+// itCompanies[0]= "Facebook";
+// itCompanies[1]= "Google";
+// itCompanies.push("Microsoft");
+// itCompanies.push("Apple");
+// itCompanies.push("IBM");
+// itCompanies.push("Oracle");
+// itCompanies.push("Amazon");
 
 
-console.log(itCompanies);
+// console.log(itCompanies);
 
-console.log(itCompanies[0],itCompanies[1]);
-console.log(itCompanies[itCompanies.length-1]);
-console.log(itCompanies[Math.floor(itCompanies.length/2)]);
+// console.log(itCompanies[0],itCompanies[1]);
+// console.log(itCompanies[itCompanies.length-1]);
+// console.log(itCompanies[Math.floor(itCompanies.length/2)]);
 
 // 10.Print out each company
 
-for (let i =0 ; i< itCompanies.length; i++){
+// for (let i =0 ; i< itCompanies.length; i++){
 
-    console.log(itCompanies[i]);
-}
+//     console.log(itCompanies[i]);
+// }
 
 // 11.Change each company name to uppercase one by one and print them out
 
-itCompanies.forEach(i =>{
-   console.log(i.toUpperCase())
-});
+// itCompanies.forEach(i =>{
+//    console.log(i.toUpperCase())
+// });
 
 
 // 12.Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
@@ -127,7 +127,7 @@ itCompanies.forEach(i =>{
 
 
 
-console.log(`${itCompanies.slice(0,6)} and ${itCompanies[itCompanies.length-1]} are big IT companies. `);
+// console.log(`${itCompanies.slice(0,6)} and ${itCompanies[itCompanies.length-1]} are big IT companies. `);
 
 // console.log(itCompanies.slice(-1))
 
@@ -205,16 +205,52 @@ console.log(`${itCompanies.slice(0,6)} and ${itCompanies[itCompanies.length-1]} 
 //     }      
 // }
 
-const result = itCompanies.filter(i => i.includes("o"));
-console.log(result)
+// const result = itCompanies.filter(i => i.includes("o"));
+// console.log(result)
 
-let yazi = "kerim";
+// let yazi = "kerim";
 
-let yazi_lengt = yazi.length
-let arr = []
-for( let i = yazi_lengt ; i>=0 ; i--){
-    arr.push(yazi[i])
+// let yazi_lengt = yazi.length
+// let arr = []
+// for( let i = yazi_lengt ; i>=0 ; i--){
+//     arr.push(yazi[i])
  
     
-}
-console.log(arr.join(""))
+// }
+// console.log(arr.join(""))
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.pop());
+
+console.log(fruits)
+
+console.log(fruits.push("Kiwi"));
+
+console.log(fruits.shift())
+console.log(fruits)
+
+
+console.log(fruits.unshift("LEMON"))
+console.log(fruits)
+
+// console.log(delete fruits[0]);
+
+// console.log(fruits)
+
+
+// const myGirls = ["Cecilie", "Lone"];
+// const myBoys = ["Emil", "Tobias", "Linus"];
+// console.log(myGirls.concat(myBoys).concat(myGirls).concat(fruits));
+// console.log(myGirls.concat(myGirls, myBoys, "kerim"))
+
+
+// console.log(fruits)
+// console.log(fruits.splice(2, 0, "Lemon", "Kiwi"));
+
+// console.log(fruits)
+// console.log(fruits.splice(0, 2));
+
+console.log(fruits)
+console.log(fruits.slice(2))
+console.log(fruits.toString())
+console.log(fruits);
